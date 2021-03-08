abd6a89f735ac391faa404b85c6e09dbe4011343
Yao Li <li@ikg.intern>
2021-03-08 18:02:59 +0100

    read data

--- group_formation/main.py
+++ group_formation/main.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec  3 17:13:31 2020
+1. read processed data
+2. give data as a streaming
+3. dynamic clustering
 
 @author: yaoli
 """
+import pandas as pd
+import numpy as np
+import os
+import time
+
+# read processed data
+sample = 'deathCircle'
+output_dir ='../data/stanford_campus_dataset/processed/'+ sample +'/'
+
+files = os.listdir(output_dir)
+# test:
+file = files[0]
+avg_info = files[1]
+print('start to deal with '+ file + '...')
+
+df = pd.read_csv(output_dir+file, delimiter=',')
+df2 = pd.read_csv(output_dir+avg_info, delimiter=',')
+
+# give data as a stream
 
